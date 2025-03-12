4bdfa2b02733d8c20cd9b3e735e928c64fb1a0cb
Vedant Kendre <vedantkendre12@gmail.com>
2025-03-12 19:41:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 import mysql.connector
 import os
 
-DB_HOST = os.getenv("postgres.railway.internal", "your-railway-host")
-DB_PORT = os.getenv("5432", "3306")
-DB_USER = os.getenv("postgres", "your-railway-user")
-DB_PASSWORD = os.getenv("MFZHfrYWlnetoIrvsmtNvxsujBMAJZZh", "your-railway-password")
-DB_NAME = os.getenv("railway", "your-database-name")
+DB_HOST = os.getenv("DB_HOST", "postgres.railway.internal")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "MFZHfrYWlnetoIrvsmtNvxsujBMAJZZh")
+DB_NAME = os.getenv("DB_NAME", "railway")
 
 conn = mysql.connector.connect(
     host=DB_HOST,
